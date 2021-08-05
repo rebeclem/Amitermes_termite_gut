e7b26b48751dd550f87c5f779cc6641675f4c72a
Rebecca Clement <33873594+rebeclem@users.noreply.github.com>
2021-08-05 09:51:05 -0600

    added short and changed to fastQC

--- fastqc.sh
+++ fastqc.sh
@@ -2,12 +2,12 @@
 #SBATCH -J fastqc
 #SBATCH -o fastqc.out
 #SBATCH -e fastqc.err
-#SBATCH -p defq
+#SBATCH -p defq, short
 #SBATCH -n 16
 #SBATCH -t 2-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rebeccaclement@gwu.edu
 
-module load fastqc 
+module load fastQC 
 
 fastqc -o outfastqc */*
