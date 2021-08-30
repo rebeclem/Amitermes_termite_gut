d77b63c4fb201d90c19ec39063b769f3abca0a1a
Rebecca Clement <33873594+rebeclem@users.noreply.github.com>
2021-08-29 19:31:30 -0600

    updated file

--- scripts/pathoscope_bacteria.sh
+++ scripts/pathoscope_bacteria.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -t 2-00:00:00
-#SBATCH -p defq,gpu,short
-#SBATCH -o PS_bac2.%A_%a.out
-#SBATCH -e PS_bac2.%A_%a.err
-#SBATCH --array=1-25
+#SBATCH -p defq,large-gpu,small-gpu
+#SBATCH -o PS_bac.%A_%a.out
+#SBATCH -e PS_bac.%A_%a.err
+#SBATCH --array=1-33
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rebeccaclement@gwu.edu
 
@@ -14,32 +14,31 @@ name=$(sed -n "$SLURM_ARRAY_TASK_ID"p ../samps.txt)
 t1=$(date +"%s")
 
 echo $name
-mkdir -p $name/bac2
+mkdir -p $name/bac
 
 module load pathoscope
 module load bowtie2
 
-
-pathoscope.py MAP -numThreads $(nproc)\
- -outDir $name/bac2 \
+pathoscope MAP -numThreads $(nproc)\
+ -outDir $name/bac \
  -indexDir ../refs \
- -targetIndexPrefixes ref_prok_rep_genomes.00_ti,ref_prok_rep_genomes.01_ti,ref_prok_rep_genomes.02_ti,ref_prok_rep_genomes.03_ti,ref_prok_rep_genomes.04_ti,ref_prok_rep_genomes.05_ti,ref_viruses_rep_genomes_ti,ref_viroids_rep_genomes_ti \
- -filterIndexPrefixes genome,phix174 \
+ -targetIndexPrefixes ref_prok_rep_genomes.00_ti,ref_prok_rep_genomes.01_ti,ref_prok_rep_genomes.02_ti,ref_prok_rep_genomes.03_ti,ref_prok_rep_genomes.06_ti,ref_prok_rep_genomes.07_ti \
+ -filterIndexPrefixes nasExi,phix174,zooNev,amiFal \
  -1 $(ls $name/flexcleaned_1.fastq) \
  -2 $(ls $name/flexcleaned_2.fastq)
- 
+
  echo "Completed running PathoMAP on bac for $name"
 
 
-pathoscope.py ID \
- -outDir $name/bac2 \
+pathoscope ID \
+ -outDir $name/bac \
  -thetaPrior 10000000 \
- -alignFile $name/bac2/outalign.sam
+ -alignFile $name/bac/outalign.sam
 
  echo "Completed running PathoID on bac for $name"
- 
+
  # this removes all unnecessary files that are taking up a lot of room.
-# rm $name/bac2/pathomap-*
+rm $name/bac/pathomap-*
 
 #---Complete job
 t2=$(date +"%s")
